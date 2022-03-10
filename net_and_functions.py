841b8649df4bfb7156e059a5e951bd3dd71062b8
hyllevask <johan.ohman@ltu.se>
2022-03-10 16:27:39 +0100

    Rewrote the code to ensure functionallity
    Now uses standard pytorch transforms only applied to the image, the labeldata i kept. Continued improvement of the commandline features and automatic logging.

--- net_and_functions.py
+++ net_and_functions.py
@@ -1,9 +1,10 @@
 from torch.utils.data import Dataset
 import torch
 import pandas as pd
+from torchvision.transforms import RandomRotation
 
 import os
-from skimage import io, transform
+from skimage import io
 import numpy as np
 
 class FishFinDataset(Dataset):
@@ -30,7 +31,8 @@ class FishFinDataset(Dataset):
 
         img_name = os.path.join(self.root_dir,
                                 self.fish_frame.iloc[idx, 0])
-        image = io.imread(img_name)
+        image = torch.tensor(io.imread(img_name)/255)
+        image = image.permute(2,0,1)
         combined_label = self.fish_frame.iloc[idx, 1]		#Scale to 0-3
         if combined_label == 1:
             label = 0
@@ -48,125 +50,13 @@ class FishFinDataset(Dataset):
         #convert_strings = {'Has_fin':0, 'No_fin':1, 'Cannot_see':2}
         #convert_strings2 = {'No Fungi': 0, 'Slight Fungi':1, 'Severe Fungi':1}
 
-        sample = {'image': image, 'label': np.array(label), 'label2': np.array(label2)}
+        
 
         if self.transform:
-            sample = self.transform(sample)
+            image = self.transform(image)
+
+        sample = {'image': image, 'label': torch.tensor([label]), 'label2': torch.tensor([label2])}
 
         return sample
 
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
 
-    def __call__(self, sample):
-        image, label, label2 = sample['image'], sample['label'], sample['label2']
-
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C x H x W
-        image = image.transpose((2, 0, 1))
-        #label = label.reshape((-1,))
-        return {'image': torch.from_numpy(image),
-                'label': torch.from_numpy(label),
-                'label2':torch.from_numpy(label2)}
-
-class ToNormTensor(object):
-    """Convert ndarrays in sample to Tensors."""
-
-    def __call__(self, sample):
-        image, label, label2 = sample['image'], sample['label'], sample['label2']
-        mm,sd = torch.tensor([0.3429,0.3551,0.3092]),torch.tensor([0.1764,0.1702,0.1767])
-
-        mm,sd = torch.reshape(mm,[3,1,1]),torch.reshape(sd,[3,1,1])
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C x H x W
-        image = image.transpose((2, 0, 1))
-        #label = label.reshape((-1,))
-        return {'image': (torch.from_numpy(image)-mm)/sd,
-                'label': torch.from_numpy(label),
-                'label2':torch.from_numpy(label2)}
-
-
-class Rescale(object):
-    """Rescale the image in a sample to a given size.
-
-    Args:
-        output_size (tuple or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
-
-    def __call__(self, sample):
-        image, label, label2 = sample['image'], sample['label'], sample['label2']
-
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * w / h
-        else:
-            new_h, new_w = self.output_size
-
-        new_h, new_w = int(new_h), int(new_w)
-
-        img = transform.resize(image, (new_h, new_w))
-
-        # h and w are swapped for landmarks because for images,
-        # x and y axes are axis 1 and 0 respectively
-        
-
-        return {'image': img, 'label': label, 'label2': label2}
-
-
-class MultiHeadResNet(torch.nn.Module):
-    def __init__(self,net):
-        super().__init__()
-        self.conv1 = list(net.children())[0]
-        self.bn1 = list(net.children())[1]
-        self.relu = list(net.children())[2]
-        self.maxpool = list(net.children())[3]
-        self.layer1 = list(net.children())[4]
-        self.layer2 = list(net.children())[5]
-        self.layer3 = list(net.children())[6]
-        self.layer4 = list(net.children())[7]
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.fc1 = nn.Linear(512,3)
-        self.fc2 = nn.Linear(512,3)
-
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x1 = self.fc1(x)
-        x2 = self.fc2(x)
-        return x1,x2
-
-
-
-#####################################
-
-#KOLLA IGENOM CUSTOMKLASSEN 
-
-
-#####################################
-
-def my_collate(batch):
-    data = [item['image'] for item in batch]
-    target = [item['label'] for item in batch]
-    target = torch.LongTensor(target)
-    return [data, target]
--- train_resnet.py
+++ train_resnet.py
@@ -20,18 +20,21 @@ import datetime
 import os
 
 #Import the user defined functions from the helper file.
-from net_and_functions import FishFinDataset, ToNormTensor, Rescale, MultiHeadResNet
+from net_and_functions import FishFinDataset
 
 
 #####################################
 
 
 # Function writes the hyperparameters to a txt file in the results folder.
-def write_hyper_parameters(folderpath,h_param):
+def write_hyper_parameters(folderpath,h_param,tfs):
     import json
     filename = folderpath + '/hyperparameters.txt'
     with open(filename,'w') as write_dict:
         write_dict.write(json.dumps(h_param))
+        write_dict.write(json.dumps(str(tfs)))
+
+
 
 def main(mode,hyper_param):
 
@@ -39,15 +42,16 @@ def main(mode,hyper_param):
     #Get current datatime and create a resultsfolder
     results_folder = datetime.datetime.now().strftime("%Y-%m-%d %X") + " Mode:"+mode
     os.mkdir(os.path.join("./results", results_folder))
-    #Write the hyperparameters to a log-file
-    write_hyper_parameters(os.path.join("./results", results_folder),hyper_param)
+    
 
 
     
-    batch_size = 8
-    rescale = Rescale((256,512))
-    tonormtensor = ToNormTensor()
-    tfs = transforms.Compose([rescale,tonormtensor])
+    batch_size = hyper_param['batch']
+
+    if hyper_param['augment']:
+        tfs = transforms.Compose([transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),transforms.RandomRotation(30,expand=True),transforms.RandomHorizontalFlip(p=0.5),transforms.Resize((256,512))])
+    else: 
+        tfs = transforms.Compose([transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)), transforms.Resize((256,512))])
 
     #Check if we are on the gpu-tower
     device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
@@ -58,6 +62,10 @@ def main(mode,hyper_param):
     image_root = "./combined_dataset/combined_images"
 
     
+    #Write the hyperparameters to a log-file
+    write_hyper_parameters(os.path.join("./results", results_folder),hyper_param,tfs)
+
+
     
     trainset = FishFinDataset(train_csv_path, image_root ,transform=tfs)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,shuffle=True, num_workers=1)
@@ -69,14 +77,12 @@ def main(mode,hyper_param):
 
 
 
-    net = torchvision.models.resnet18(pretrained=False)
+    myNet = torchvision.models.resnet18(pretrained=False)
 
-    if mode == "joint":
-        myNet = MultiHeadResNet(net)
-    else:
-        net.fc= nn.Linear(512, 1) #Labeling has fin/no fin/unsure  ResNet18
-        #net.fc= nn.Linear(2048, 2) #Labeling has fin/no fin/unsure  RwsNet50
-        myNet = net 
+  
+    myNet.fc= nn.Linear(512, 1) #ResNet18
+    #net.fc= nn.Linear(2048, 2) #RwsNet50
+     
 
  
     myNet.float()
@@ -86,100 +92,77 @@ def main(mode,hyper_param):
 
     #criterion = nn.CrossEntropyLoss()  #For MultiClass
     criterion = nn.BCEWithLogitsLoss()  #For binary
-    optimizer = optim.Adam(myNet.parameters(),lr = 0.02)
-    scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=10,gamma=0.5)
+    optimizer = optim.Adam(myNet.parameters(),lr = hyper_param['lr'])
     
-
+    #Setup tensorboard 
     writer = SummaryWriter(os.path.join("./results", results_folder))
     n_total_steps = len(trainloader)
 
     #Main training loop
+    myNet.train()
     for epoch in range(hyper_param['epochs']):
         running_loss = 0
         for i,data in enumerate(tqdm(trainloader)):
             
-            image = data['image']
-            label = data['label']
-            label2 = data['label2']
+            image = data['image'].float().to(device)
+            if mode == "fin":
+                target = data['label'].float().to(device)
+            elif mode == "fungi":
+                target = data['label2'].float().to(device)
             optimizer.zero_grad()
 
-            if mode == "joint":
-                out1,out2 = myNet(image.float().to(device))
-            
-                loss1 = criterion(out1,label.to(device))
-                loss2 = criterion(out2,label2.to(device))
-                loss = loss1+loss2
-            elif mode == "fin":
-                out = myNet(image.float().to(device))
-                loss = criterion(out,label.to(device))
-            elif mode == "fungi":
-                out = myNet(image.float().to(device))
-                loss = criterion(out[:,-1],label2.float().to(device))
+
+            out = myNet(image)
+            loss = criterion(out,target)
 
             running_loss += loss.item()
 
             loss.backward()
             optimizer.step()
-            #print(data)
+
             if (i+1) % 50 == 0:
-                writer.add_scalar('training loss', running_loss / 100, epoch*n_total_steps + i)
+                writer.add_scalar('training loss', running_loss / 50, epoch*n_total_steps + i)
                 running_loss = 0.0
-                
+        #Save this epochs last weights
         torch.save(myNet.state_dict(), "./results/" + results_folder + "/epoch_" + str(epoch)+".pt")
+
+
+        ##################################
+        ##          VALIDATION          ##
+        ##################################
+
+        #Total Validation Loss
         validation_total = 0
-        validation1 = 0
-        validation2 = 0
 
-        correct2 = 0
-        correct1 = 0
-        total1 = 0
-        total2 = 0
+        #Number of total validation samples
         total = 0
+
+        #Number of corect classified samples 
         correct = 0
+
+
         #Run inference on validation set
+        myNet.eval()
         for j,data in enumerate(tqdm(validationloader)):
-            image = data['image']
-            label = data['label']
-            label2 = data['label2']
-            if mode == "joint":
-
-                out1,out2 = myNet(image.float().to(device))
-            
-                loss1 = criterion(out1,label.to(device))
-                loss2 = criterion(out2,label2.to(device))
-                loss = loss1+loss2
-                validation1 += loss1.item()
-                validation2 += loss2.item()
-
-                total1 +=  len(label)
-                correct1 += (torch.argmax(out1,axis=1) == label.to(device)).sum().item() 
-                correct2 += (torch.argmax(out2,axis=1) == label2.to(device)).sum().item()
-                total2 += len(label2)
-            elif mode == "fin":
-                out = myNet(image.float().to(device))
-                loss = criterion(out,label.to(device))
-                total +=  len(label)
-                correct += (torch.argmax(out,axis=1) == label.to(device)).sum().item()
+            image = data['image'].float().to(device)
+            if mode == "fin":
+                target = data['label'].float().to(device)
             elif mode == "fungi":
-                out = myNet(image.float().to(device))
-                loss = criterion(out[:,-1],label2.float().to(device))
+                target = data['label2'].float().to(device)
 
-                total +=  len(label)
-                correct += (torch.round(torch.sigmoid(out[:,-1])) == label2.to(device)).sum().item()
+            out = myNet(image)
+            loss = criterion(out,target)
+
+            total +=  len(target)
+            correct += (torch.round(torch.sigmoid(out)) == target.to(device)).sum().item()
             
             validation_total += loss.item()
             
         
-        if mode == "joint":
-            writer.add_scalar('Validation Loss/Total', validation_total / len(validationset), epoch)
-            writer.add_scalar('Validation Loss/Label1', validation1 / len(validationset), epoch)
-            writer.add_scalar('Validation Loss/Label2', validation2 / len(validationset), epoch)
-            writer.add_scalar('Validation Accuracy/Label1',correct1/total1, epoch)
-            writer.add_scalar('Validation Accuracy/Label2',correct2/total2, epoch)
-        else:
-            writer.add_scalar('Validation Loss', validation_total / len(validationset)*batch_size, epoch)   #Loss per batch
-            writer.add_scalar('Validation Accuracy', correct/total, epoch)
-        scheduler.step()
+
+        writer.add_scalar('Validation Loss', validation_total / len(validationset)*batch_size, epoch)   #Loss per batch
+        writer.add_scalar('Validation Accuracy', correct/total, epoch)
+    
 
 
 
@@ -192,17 +175,18 @@ def main(mode,hyper_param):
 
 
 if __name__ == "__main__":
-    debug = 1
+    debug = 0
     if debug == 1:
         mode ='fungi'
         h_param = {
             'epochs': 100,
             'lr': 0.01,
             'load': None,
-            'batch': 8
+            'batch': 8,
+            'augment': True
             }
     else:
-
+        print("Using parsed data")
         import argparse
         parser = argparse.ArgumentParser(description="Train script for the first fish dataset")
         parser.add_argument('--mode',type=str,required=True)
@@ -210,6 +194,7 @@ if __name__ == "__main__":
         parser.add_argument('--epochs',type=int,default=30)
         parser.add_argument('--load',type=str,default=None)
         parser.add_argument('--batch',type=int,default=8)
+        parser.add_argument('--augment',type=bool,default=False)
 
         args = parser.parse_args()
         mode = args.mode
@@ -218,7 +203,8 @@ if __name__ == "__main__":
             'lr': args.lr,
             'epochs': args.epochs,
             'load': args.load,
-            'batch': args.batch
+            'batch': args.batch,
+            'augment': args.augment
         }
 
     allowed_modes = ["fin", "fungi","joint"]
